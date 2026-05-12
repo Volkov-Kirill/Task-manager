f561905929b11c806581305f6a296ce28e9bd358
nikitaToner <x1tle@yandex.ru>
2026-05-12 07:18:15 +0300

    Update TaskItem.cs

--- TaskManager/Models/TaskItem.cs
+++ TaskManager/Models/TaskItem.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskManager.Models
 {
-    internal class TaskItem
+    public class TaskItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public int CreatedByUserId { get; set; }
+        public virtual User CreatedByUser { get; set; }
+
+        public int? AssignedToUserId { get; set; }
+        public virtual User AssignedToUser { get; set; }
+
+        public bool IsForAllWorkers { get; set; }
+
+        [NotMapped]
+        public string ReceiverName
+        {
+            get
+            {
+                if (IsForAllWorkers)
+                    return "Все работники";
+
+                if (AssignedToUserId == CreatedByUserId)
+                    return "Личная";
+
+                if (AssignedToUser != null)
+                    return AssignedToUser.Name;
+
+                return "Не указано";
+            }
+        }
     }
 }
